6f3e604dc282e7badde73af86d803999949074fb
MD ALI SHEKH <mdalishekh2003@gmail.com>
2024-09-18 13:22:24 +0530

    Netlify cors added

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ origins = [
     "http://127.0.0.1:5500",  # Your frontend URL
     "http://localhost:5500",
     "https://gocab.netlify.app",
+    "https://gocab.netlify.app/",
     # You can add more origins here if needed
 ]
 
