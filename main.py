21f1c0914ca56756455790c3f22fd32488c21b08
MD ALI SHEKH <mdalishekh2003@gmail.com>
2024-09-21 19:22:01 +0530

    Added more CORS

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ app = FastAPI()
 origins = [
     "http://127.0.0.1:5500",  # Your frontend URL
     "http://localhost:5500",
+    "http://127.0.0.1:5501",
     "https://gocab.netlify.app",
     "https://gocab.netlify.app/",
     # You can add more origins here if needed
