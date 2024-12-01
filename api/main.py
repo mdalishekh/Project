1c1bd6ed12a5f80b16b7500a62b90fe378981dd2
MD ALI SHEKH <mdalishekh2003@gmail.com>
2024-12-01 12:34:54 +0530

    Test Application for Vercel Deploy

---
+++ api/main.py
@@ -0,0 +1,39 @@
+# Importing some useful Packages to be used in this project 
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
+
+# Initiating an instance for FastAPI 
+app = FastAPI()
+
+# Define the origins you want to allow
+origins = [
+    "http://127.0.0.1:5500",  # Your frontend URL
+    "http://localhost:5500",
+    "http://127.0.0.1:5501",
+    "http://127.0.0.1:5500/",
+    "https://gocab.netlify.app",
+    "https://gocab.netlify.app/",
+    # You can add more origins here if needed
+]
+
+
+# Add the CORS middleware to the FastAPI application
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Origins allowed to make requests
+    allow_credentials=True,  # Allow cookies to be sent with requests
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+# Creating a route for testing our REST API with GET method
+@app.get('/test-api')
+def test_api():
+    print("----------------------")
+    print("\tServer is active")
+    print("----------------------")
+    return JSONResponse({"response": "Our Server is active"})
+ 
